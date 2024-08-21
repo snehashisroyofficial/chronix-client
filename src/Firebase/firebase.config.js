e7e4da77604b826071e2016fa89520aa32d618ba
snehashisroyofficial <snehashisroyofficial@gmail.com>
2024-08-21 22:13:21 +0530

    final push

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD6apGck7B6Q3gCEYX_WA8eSfBMXri0DEA",
-  authDomain: "chronix-76387.firebaseapp.com",
-  projectId: "chronix-76387",
-  storageBucket: "chronix-76387.appspot.com",
-  messagingSenderId: "1005642602655",
-  appId: "1:1005642602655:web:ee25f6f6dd0b859a486aaa",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
--- src/Pages/AllProducts.jsx
+++ src/Pages/AllProducts.jsx
@@ -49,7 +49,7 @@ const AllProducts = () => {
 
   if (loading) {
     return (
-      <div className="min-h-screen flex justify-center items-center">
+      <div className="h-screen flex justify-center items-center">
         <PacmanLoader color="#0019ff" />
       </div>
     );
